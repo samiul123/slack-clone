7e30d15a502d265656e7c7d11ed8bbda7f781ca5
Sami <samiulmushfik123@gmail.com>
2020-08-19 00:27:11 +0600

    Add sidebar header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="app">
       <Header/>
+      <div class="app__body">
+        <Sidebar/>
+      </div>
+    
     </div>
   );
 }
