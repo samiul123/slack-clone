ebde07b547a7d7eb294046b0f7c38eb625816d59
Sami <samiulmushfik123@gmail.com>
2020-08-28 21:44:36 +0600

    Make chat header dynamic

--- src/Chat.js
+++ src/Chat.js
@@ -1,28 +1,39 @@
-import React from 'react'
-import "./Chat.css"
-import { useParams } from "react-router-dom";
-import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
 import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "./Chat.css";
+import db from "./firebase";
 
 function Chat() {
-    const { roomId } = useParams();
-    return (
-        <div className="chat">
-            <div className="chat__header">
-                <div className="chat__headerLeft">
-                    <h4 className="chat__channelName">
-                        <strong>#general</strong>
-                        <StarBorderOutlinedIcon/>
-                    </h4>
-                </div>
-                <div className="chat__headerRight">
-                    <p>
-                        <InfoOutlinedIcon/> Details
-                    </p>
-                </div>
-            </div>
+  const { roomId } = useParams();
+  const [roomDetails, setRoomDetails] = useState([]);
+
+  useEffect(() => {
+    if (roomId) {
+      db.collection("rooms")
+        .doc(roomId)
+        .onSnapshot((snapshot) => setRoomDetails(snapshot.data()));
+    }
+  }, [roomId]);
+
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <div className="chat__headerLeft">
+          <h4 className="chat__channelName">
+            <strong>#{roomDetails.name}</strong>
+            <StarBorderOutlinedIcon />
+          </h4>
         </div>
-    )
+        <div className="chat__headerRight">
+          <p>
+            <InfoOutlinedIcon /> Details
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Chat
+export default Chat;
